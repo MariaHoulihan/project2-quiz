6c6f40b390ffbd29eefadc15082bc721144150b0
MariaHoulihan <mariahoulihan@gmail.com>
2023-02-15 22:15:33 +0000

    Add questions as a var

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+var questions = [{
+  
+    question: "The occipital area of the skull is located in what part of the skull?",
+    choices : ["Front", "Back", "Top"],
+    correstAnswer: 1
+  },
+  {
+    question: "The biceps femoris muscle is located in what part of the body?",
+    choices: ["Shoulder", "Arm", "Leg"],
+    correctAnswer: 2
+  },
+  {
+    question: "The elbow is an example of what type of joint?",
+    choices: ["Pivot joint", "Hinge joint", "Ball and Socket joint"],
+    correctAnswer: 1
+  },
+   { 
+    question: "The muscles located at the front of the thigh are known as what?",
+    choices: ["Adductors", "Hamstrings", "Quadriceps"],
+     correctAnswer: 2
+   },
+   {
+     question: "The C1 vertebra is called?",
+     choices: ["Atlas", "Axis", "Amino"],
+     correctAnswer: 0
+   }, 
+   {
+     question: "The three bones that form the ankle are the fibula, tibia and which other bone?",
+     choices: ["Talus", "Calcaneous", "Cuboid"],
+     correctAnswer: 0
+   },
+   {
+     question: "Which vessel returns unoxygenated blood to the lungs?",
+     choices: ["Aorta", "Pulmonary artery", "Pulmonary vein"],
+     correctAnswer: 1
+   }, 
+    {
+      question: "Ligaments and Tendons are composed of what type of tissue?",
+      choices: ["Epithelial", "Adipose", “Connective”],
+      correctAnswer: 2
+    },
+     {
+       question: "The anatomical term for the head is?",
+       choices: ["Cephalic", "Cervical", "Thoracic"],
+       correctAnswer: 0
+     }, 
+      {
+        question: "Mastication is the term for what?",
+        choices: ["Chewing", "Snoring", "Swallowing"],
+        correctAnswer: 0
+      }  ];
+  
