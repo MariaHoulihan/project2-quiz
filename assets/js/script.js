ed938ada621aa1ec2ceefe502808d08d5b334721
MariaHoulihan <mariahoulihan@gmail.com>
2023-02-16 22:57:35 +0000

    Add some editing to html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
   
   let buttons = document.getElementsByTagName("button");
 
@@ -18,61 +18,82 @@ function playGame(playerChoice) {
 function updateScores() {
   
 }
-
-/* var or let ???? */
-var questions = [{
-  
-    question: "The occipital area of the skull is located in what part of the skull?",
-    options: ["Front", "Back", "Top"],
-    correstAnswer: 1
+*/
+let questions = [
+  {
+    question: 'The occipital area of the skull is located in what part of the skull?',
+    option1: 'Front',
+    option2: 'Back',
+    option3: 'Top',
+    answer: 'Back',
   },
   {
-    question: "The biceps femoris muscle is located in what part of the body?",
-    options: ["Shoulder", "Arm", "Leg"],
-    correctAnswer: 2
+    question: 'The biceps femoris muscle is located in what part of the body?',
+    option1: 'Shoulder', 
+    option2: 'Arm',
+    option3: 'Leg',
+    answer: 'Leg',
   },
   {
-    question: "The elbow is an example of what type of joint?",
-    options: ["Pivot joint", "Hinge joint", "Ball and Socket joint"],
-    correctAnswer: 1
+    question: 'The elbow is an example of what type of joint?',
+    option1: 'Pivot joint',
+    option2: 'Hinge joint',
+    option3: 'Ball and Socket joint',
+    answer: 'Hinge joint',
   },
    { 
-    question: "The muscles located at the front of the thigh are known as what?",
-    options: ["Adductors", "Hamstrings", "Quadriceps"],
-     correctAnswer: 2
+    question: 'The muscles located at the front of the thigh are known as what?',
+    option1: 'Adductors', 
+    option2: 'Hamstrings', 
+    option3: 'Quadriceps',
+    answer: 'Quadriceps',
    },
    {
-     question: "The C1 vertebra is called?",
-     options: ["Atlas", "Axis", "Amino"],
-     correctAnswer: 0
+    question: 'The C1 vertebra is called?',
+    option1: 'Atlas',
+    option2: 'Axis',
+    option3: 'Amino',
+    answer: 'Atlas'
    }, 
    {
-     question: "The three bones that form the ankle are the fibula, tibia and which other bone?",
-     options: ["Talus", "Calcaneous", "Cuboid"],
-     correctAnswer: 0
+    question: 'The three bones that form the ankle are the fibula, tibia and which other bone?',
+    option1: 'Talus',
+    option2: 'Calcaneous',
+    option3: 'Cuboid',
+    answer: 'Talus',
    },
    {
-     question: "Which vessel returns unoxygenated blood to the lungs?",
-     options: ["Aorta", "Pulmonary artery", "Pulmonary vein"],
-     correctAnswer: 1
+    question: 'Which vessel returns unoxygenated blood to the lungs?',
+    option1: 'Aorta',
+    option2: 'Pulmonary artery',
+    option3: 'Pulmonary vein',
+    answer: 'Pulmonary artery'
    }, 
     {
-      question: "Ligaments and Tendons are composed of what type of tissue?",
-      options: ["Epithelial", "Adipose", “Connective”],
-      correctAnswer: 2
+    question: 'Ligaments and Tendons are composed of what type of tissue?',
+    option1: 'Epithelial',
+    option2: 'Adipose',
+    option3: 'Connective',
+    answer: 'Connective',
     },
      {
-       question: "The anatomical term for the head is?",
-       options: ["Cephalic", "Cervical", "Thoracic"],
-       correctAnswer: 0
+    question: 'The anatomical term for the head is?',
+    option1: 'Cephalic',
+    option2: 'Cervical',
+    option3: 'Thoracic',
+    answer: 'Cephalic',
      }, 
       {
-        question: "Mastication is the term for what?",
-        options: ["Chewing", "Snoring", "Swallowing"],
-        correctAnswer: 0
-      }  ];
-  
+    question: 'Mastication is the term for what?',
+    option1: 'Chewing',
+    option2: 'Snoring',
+    option3: 'Swallowing',
+    answer: 'Chewing'
+      }  
+    ];
+/*  
 function gameArea() {
 
 }
 
+*/
