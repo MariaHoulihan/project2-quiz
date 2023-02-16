b90d9507944199fef672bf85dbe36134233b29f5
MariaHoulihan <mariahoulihan@gmail.com>
2023-02-16 16:59:55 +0000

    Add submit answer button and style for submit answer button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,71 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of my buttons) {
+    button.addEventListener("click"), function() {
+      if (this.getAttribute("data-type") === "submit")
+    }
+      alert("You clicked Submit!");
+  }
+
+}
+)}
+}
+/* var or let ???? */
 var questions = [{
   
     question: "The occipital area of the skull is located in what part of the skull?",
-    choices : ["Front", "Back", "Top"],
+    options: ["Front", "Back", "Top"],
     correstAnswer: 1
   },
   {
     question: "The biceps femoris muscle is located in what part of the body?",
-    choices: ["Shoulder", "Arm", "Leg"],
+    options: ["Shoulder", "Arm", "Leg"],
     correctAnswer: 2
   },
   {
     question: "The elbow is an example of what type of joint?",
-    choices: ["Pivot joint", "Hinge joint", "Ball and Socket joint"],
+    options: ["Pivot joint", "Hinge joint", "Ball and Socket joint"],
     correctAnswer: 1
   },
    { 
     question: "The muscles located at the front of the thigh are known as what?",
-    choices: ["Adductors", "Hamstrings", "Quadriceps"],
+    options: ["Adductors", "Hamstrings", "Quadriceps"],
      correctAnswer: 2
    },
    {
      question: "The C1 vertebra is called?",
-     choices: ["Atlas", "Axis", "Amino"],
+     options: ["Atlas", "Axis", "Amino"],
      correctAnswer: 0
    }, 
    {
      question: "The three bones that form the ankle are the fibula, tibia and which other bone?",
-     choices: ["Talus", "Calcaneous", "Cuboid"],
+     options: ["Talus", "Calcaneous", "Cuboid"],
      correctAnswer: 0
    },
    {
      question: "Which vessel returns unoxygenated blood to the lungs?",
-     choices: ["Aorta", "Pulmonary artery", "Pulmonary vein"],
+     options: ["Aorta", "Pulmonary artery", "Pulmonary vein"],
      correctAnswer: 1
    }, 
     {
       question: "Ligaments and Tendons are composed of what type of tissue?",
-      choices: ["Epithelial", "Adipose", “Connective”],
+      options: ["Epithelial", "Adipose", “Connective”],
       correctAnswer: 2
     },
      {
        question: "The anatomical term for the head is?",
-       choices: ["Cephalic", "Cervical", "Thoracic"],
+       options: ["Cephalic", "Cervical", "Thoracic"],
        correctAnswer: 0
      }, 
       {
         question: "Mastication is the term for what?",
-        choices: ["Chewing", "Snoring", "Swallowing"],
+        options: ["Chewing", "Snoring", "Swallowing"],
         correctAnswer: 0
       }  ];
   
+function gameArea() {
+
+}
+
